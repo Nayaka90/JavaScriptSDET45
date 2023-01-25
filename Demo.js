40b3508718e421d9f2ac7d4494522d7701beee85
Nithishnayaka <nithishnyaka90@gmail.com>
2023-01-25 17:35:14 +0530

    included comments in it

--- Demo.js
+++ Demo.js
@@ -1,7 +1,7 @@
 var a=" Nithish"
-var a="jagadeesh"
+var a="jagadeesh"//we can redeclare and reintialize
 let b=56
-b=66
-const c=66
-console.log(typeof(a))
-document.write("nithish")
+    b=66 // we cannot declare again but we can reintialize
+const c=66 // we cannot reintialize and redeclare
+console.log(typeof(a)) //this will reflect in console(developer side)
+document.write("nithish")//this will reflect on webpages(client side)
